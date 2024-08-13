b3886496aba91563dba529b7a4fb5fcb49e34a9e
SimranKamboj320 <simrankamboj320@gmail.com>
2024-08-14 01:25:31 +0530

    Spiral Matrix

---
+++ SpiralMatrix.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class SpiralMatrix{
+    public static void Spiral(int matrix[][]){
+        int startRow = 0;
+        int endRow = matrix.length-1;
+        int startCol = 0;
+        int endCol = matrix[0].length;
+
+        while(startRow <= endRow && startCol <= endCol){
+
+            for(int j=startCol; j<=endCol; j++){
+                System.out.print(matrix[startRow][j]+" ");
+            }
+            for(int i=startRow+1; i<=endRow; i++){
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            for(int j=endCol-1; j>=startCol; j--){
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            for(int i=endRow-1; i>=startRow+1; i--){
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        Spiral(matrix);
+    }
+}
