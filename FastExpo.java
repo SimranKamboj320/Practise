b47e7739d53e31f28c48e6bea9a3eb2b44b576ab
SimranKamboj320 <simrankamboj320@gmail.com>
2024-10-02 11:44:45 +0530

    Fast exponentiation
    Fast exponentiation

---
+++ FastExpo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FastExpo{
+    public static int Fast(int a, int n){
+        int ans = 1;
+
+        while(n>0){
+            if((n&1) != 0){
+                ans = ans*a;
+            }
+            a = a*a;
+            n = n>>1;
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        System.out.println(Fast(2,5));
+    }
+}
