25844342d84e1fe2ae455f803cc436992ce236f3
SimranKamboj320 <simrankamboj320@gmail.com>
2024-12-18 16:30:49 +0530

    Linked list

---
+++ LL.java
@@ -0,0 +1,52 @@
+class LL {
+    Node head;
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void AddFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;  
+    }
+    public void AddLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+    public void printLL(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null){
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+    public static void main(String args[]){
+        LL list = new LL();
+        list.AddFirst(1);
+        list.AddFirst(2);
+
+        list.printLL();
+    }  
+}
